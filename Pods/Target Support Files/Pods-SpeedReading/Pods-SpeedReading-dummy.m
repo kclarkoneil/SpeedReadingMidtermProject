66171d01589ac3383a3eb0a4a5081c9bddfec8a8
Bennett Cheung <bennettcheung@yahoo.com>
2018-08-23 11:45:31 -0700

    Added cocoapods support for realm and google material

---
+++ Pods/Target Support Files/Pods-SpeedReading/Pods-SpeedReading-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpeedReading : NSObject
+@end
+@implementation PodsDummy_Pods_SpeedReading
+@end
---
+++ Pods/Target Support Files/Pods-SpeedReadingTests/Pods-SpeedReadingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpeedReadingTests : NSObject
+@end
+@implementation PodsDummy_Pods_SpeedReadingTests
+@end
